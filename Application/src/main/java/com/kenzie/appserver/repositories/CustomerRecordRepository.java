4b0ccb19d028434f11275aae065c3d78e9fbf456
aliciaortiz2022 <110409201+aliciaortiz2022@users.noreply.github.com>
2023-03-21 12:16:14 -0600

    customerrecordrepository

--- Application/src/main/java/com/kenzie/appserver/repositories/CustomerRecordRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/CustomerRecordRepository.java
@@ -1,10 +1,14 @@
 package com.kenzie.appserver.repositories;
 
+import com.kenzie.appserver.repositories.model.CustomerRecord;
 import com.kenzie.appserver.repositories.model.ExampleRecord;
 
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 @EnableScan
-public interface ExampleRepository extends CrudRepository<ExampleRecord, String> {
+public interface CustomerRecordRepository extends CrudRepository<CustomerRecord, String> {
+    List<CustomerRecord> getUserId(String userId);
 }
