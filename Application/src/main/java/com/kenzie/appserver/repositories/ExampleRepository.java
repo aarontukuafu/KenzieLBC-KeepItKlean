982230f4a74c1caf140e2fabbb7b4c237c96bbd4
aliciaortiz2022 <110409201+aliciaortiz2022@users.noreply.github.com>
2023-03-21 19:31:04 -0600

    push repository

--- Application/src/main/java/com/kenzie/appserver/repositories/ExampleRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/ExampleRepository.java
@@ -1,14 +1,10 @@
 package com.kenzie.appserver.repositories;
 
-import com.kenzie.appserver.repositories.model.CustomerRecord;
 import com.kenzie.appserver.repositories.model.ExampleRecord;
+
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 @EnableScan
-public interface CustomerRecordRepository extends CrudRepository<CustomerRecord, String> {
-    List<CustomerRecord> getCustomerByUserId (String userId);
+public interface ExampleRepository extends CrudRepository<ExampleRecord, String> {
 }
-
