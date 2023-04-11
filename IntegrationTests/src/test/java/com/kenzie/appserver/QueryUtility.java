1b2dd758faa0b619c8c8bd9b9f986ba61123f4f3
Aaron Tukuafu <aaron.tukuafu@mykenzie.snhu.edu>
2023-04-10 21:46:55 -0700

    New Integration Test

---
+++ IntegrationTests/src/test/java/com/kenzie/appserver/QueryUtility.java
@@ -0,0 +1,50 @@
+package com.kenzie.appserver;
+
+import com.kenzie.appserver.controller.model.CustomerCreateRequest;
+import com.kenzie.appserver.controller.model.CustomerUpdateRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.testcontainers.shaded.com.github.dockerjava.core.MediaType;
+
+import static io.micrometer.core.instrument.config.MeterFilter.accept;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+public class QueryUtility {
+
+    public CustomerControllerClient customerControllerClient;
+
+    private final MockMvc mvc;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public QueryUtility(MockMvc mvc){
+        this.mvc = mvc;
+        this.customerControllerClient = new CustomerControllerClient();
+    }
+
+    public class CustomerControllerClient{
+        public ResultActions getCustomer(String userId) throws Exception{
+            return mvc.perform(get("/example/{userId}", userId)
+                .accept(String.valueOf(MediaType.APPLICATION_JSON)));
+        }
+
+        public ResultActions addNewCustomer(CustomerCreateRequest customerCreateRequest) throws Exception{
+            return mvc.perform(post("/example")
+                    .accept(String.valueOf(MediaType.APPLICATION_JSON))
+                    .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                    .content(mapper.writeValueAsString(customerCreateRequest)));
+        }
+
+        public ResultActions updateCustomer(CustomerUpdateRequest customerUpdateRequest) throws Exception{
+            return mvc.perform(put("/example")
+                    .accept(String.valueOf(MediaType.APPLICATION_JSON))
+                    .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                    .content(mapper.writeValueAsString(customerUpdateRequest)));
+        }
+
+        public ResultActions cancelSubscription(String userId) throws Exception{
+            return mvc.perform(delete("/example/{userId}/cancel", userId)
+                    .accept(String.valueOf(MediaType.APPLICATION_JSON)));
+        }
+    }
+}
